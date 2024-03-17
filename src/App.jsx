d47d73dc80966383acf822494b52f9bc0e32963a
Dilshodbek8 <20020905xd@gmail.com>
2024-03-17 12:24:01 +0500

    layout starter

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { Route, Routes } from "react-router-dom"
-import Layout from "./Layout/Layout"
-import Home from "./Home/Home"
-
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
 
 function App() {
-
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
     </Layout>
-  )
+  );
 }
 
-export default App
+export default App;
